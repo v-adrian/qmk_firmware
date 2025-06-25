adac96acc79be165ed20f0dda6c8de55c10821c2
Vonn Adrian <vonnadrianjutar123@gmail.com>
2025-06-25 12:13:19 +0800

    Turn hrm to custom bindings

--- keyboards/silakka54/keymaps/vadrian/keymap.c
+++ keyboards/silakka54/keymaps/vadrian/keymap.c
@@ -3,11 +3,24 @@
 
 #include QMK_KEYBOARD_H
 
-#define UNDO C(KC_Z)
-#define CUT C(KC_X)
-#define COPY C(KC_C)
-#define PASTE C(KC_P)
-#define REDO C(KC_Y)
+// Windows shortcuts
+#define C_UNDO C(KC_Z)
+#define C_CUT C(KC_X)
+#define C_COPY C(KC_C)
+#define C_PASTE C(KC_P)
+#define C_REDO C(KC_Y)
+
+// HRM keys
+#define HRM_A LGUI_T(KC_A)
+#define HRM_S LALT_T(KC_S)
+#define HRM_D LCTL_T(KC_D)
+#define HRM_F LSFT_T(KC_F)
+#define HRM_J RSFT_T(KC_J)
+#define HRM_K RCTL_T(KC_K)
+#define HRM_L RALT_T(KC_L)
+#define HRM_SC RGUI_T(KC_SCLN)
+
+#define C_ESC LCTL_T(KC_ESC)
 
 enum layers {
     BASE,
@@ -25,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
         KC_GRV,         KC_1,         KC_2,         KC_3,             KC_4,           KC_5,             KC_6,           KC_7,            KC_8,          KC_9,         KC_0,            KC_MINS,
         KC_TAB,         KC_Q,         KC_W,         KC_E,             KC_R,           KC_T,             KC_Y,           KC_U,            KC_I,          KC_O,         KC_P,            KC_EQL,
-        LCTL_T(KC_ESC), LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D),     LSFT_T(KC_F),   KC_G,             KC_H,           RSFT_T(KC_J),    RCTL_T(KC_K),  RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
+        C_ESC, HRM_A, HRM_S, HRM_D,     HRM_F,   KC_G,             KC_H,           HRM_J,    HRM_K,  HRM_L, HRM_SC, KC_QUOT,
         CW_TOGG,        KC_Z,         KC_X,         KC_C,             KC_V,           KC_B,             KC_N,           KC_M,            KC_COMM,       KC_DOT,       KC_SLSH,         TG(GAME),
                                                     LT(MEDIA,KC_ESC), LT(NAV,KC_SPC), LT(MOUSE,KC_TAB), LT(SYM,KC_ENT), LT(NUM,KC_BSPC), LT(FUN,KC_DEL)
     ),
@@ -47,14 +60,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_HOME, KC_UP,   KC_END,   KC_PGUP, KC_TRNS,
         KC_TRNS, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_TRNS, KC_CAPS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, SELWORD,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, REDO,    PASTE,   COPY,    CUT,      UNDO,    KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, C_REDO,    C_PASTE,   C_COPY,    C_CUT,      C_UNDO,    KC_TRNS,
                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT,  KC_BSPC, KC_DEL
     ),
     [MOUSE] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MS_WHLL, MS_UP,   MS_WHLR, MS_WHLU, KC_TRNS,
         KC_TRNS, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_TRNS, KC_TRNS, MS_LEFT, MS_DOWN, MS_RGHT, MS_WHLD, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, REDO,    PASTE,   COPY,    CUT,     UNDO,    KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, C_REDO,    C_PASTE,   C_COPY,    C_CUT,     C_UNDO,    KC_TRNS,
                                    KC_TRNS, KC_TRNS, KC_TRNS, MS_BTN2, MS_BTN1, MS_BTN3
     ),
     [SYM] = LAYOUT(
